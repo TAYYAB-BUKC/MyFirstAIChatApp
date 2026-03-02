3a68a983f7003b2028a1b58c6a0be753a12459df
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-03-03 03:03:32 +0500

    ChatAppBackgroundService added in DI container.

--- MyFirstAIChatApp/Program.cs
+++ MyFirstAIChatApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MyFirstAIChatApp;
 
 var host = Host.CreateApplicationBuilder(args);
 
@@ -9,6 +11,10 @@ var endpoint = host.Configuration["Chat:AI:Endpoint"] ?? throw new InvalidOperat
 
 var apiKey = host.Configuration["Chat:AI:ApiKey"] ?? throw new InvalidOperationException("Missing configuration: ApiKey. See the README for details");
 
+host.Services.AddHostedService<ChatApp>();
+
 var app = host.Build();
 
 Console.WriteLine($"{endpoint} - {apiKey}");
+
+await app.RunAsync();
