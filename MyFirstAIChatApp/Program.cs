53a7aef229bd13961f796f0a9ad0973e1125f56b
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-03-03 04:09:36 +0500

    Ollama configured in DI container.

--- MyFirstAIChatApp/Program.cs
+++ MyFirstAIChatApp/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MyFirstAIChatApp;
+using OllamaSharp;
 
 var host = Host.CreateApplicationBuilder(args);
 
@@ -10,6 +11,11 @@ host.Configuration.AddUserSecrets<Program>();
 var endpoint = host.Configuration["Chat:AI:Endpoint"] ?? throw new InvalidOperationException("Missing configuration: Endpoint. See the README for details");
 
 var apiKey = host.Configuration["Chat:AI:ApiKey"] ?? throw new InvalidOperationException("Missing configuration: ApiKey. See the README for details");
+
+var ollamaEndpoint = host.Configuration["Chat:AI:OllamaEndpoint"] ?? throw new InvalidOperationException("Missing configuration: ApiKey. See the README for details");
+var uri = new Uri(ollamaEndpoint);
+var ollama = new OllamaApiClient(uri);
+host.Services.AddChatClient(ollama);
 
 host.Services.AddHostedService<ChatApp>();
 
