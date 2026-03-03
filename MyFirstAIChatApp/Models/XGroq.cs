0e44ed846826fec0e082c4ed33ada5816a3315fc
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-03-04 04:21:28 +0500

    GroqUsage and XGroq models added.

---
+++ MyFirstAIChatApp/Models/XGroq.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace MyFirstAIChatApp.Models
+{
+	public class XGroq
+	{
+		[JsonProperty("id")]
+		public string Id { get; set; }
+
+		[JsonProperty("seed")]
+		public int Seed { get; set; }
+	}
+}
