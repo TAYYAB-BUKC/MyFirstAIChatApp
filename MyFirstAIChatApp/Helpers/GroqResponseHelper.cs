4ace4d7b117ddac8900aa76e038d3cdddebab4c6
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-03-04 05:35:07 +0500

    GroqResponse helper implemented.

---
+++ MyFirstAIChatApp/Helpers/GroqResponseHelper.cs
@@ -0,0 +1,32 @@
+﻿using MyFirstAIChatApp.Models;
+using Newtonsoft.Json;
+
+namespace MyFirstAIChatApp.Helpers
+{
+	public class GroqResponseHelper
+	{
+		public static GroqMessage GetLatestMessage(string jsonString)
+		{
+			GroqMessage latestMessage = null!;
+			try
+			{
+				GroqResponse response = JsonConvert.DeserializeObject<GroqResponse>(jsonString);
+
+				if (response != null && response.Choices != null && response.Choices.Length > 0)
+				{
+					latestMessage = response.Choices[0].Message;
+				}
+				else
+				{
+					Console.WriteLine("No message found in the response.");
+				}
+			}
+			catch (JsonException ex)
+			{
+				Console.Error.WriteLine($"JSON Deserialization Error: {ex.Message}");
+			}
+
+			return latestMessage;
+		}
+	}
+}
